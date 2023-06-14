0f6ad27269c1b2d0ca7efe1cc2d0b27e2a6cbdd0
moarshad100 <mo.arshad100@gmail.com>
2023-06-14 22:05:10 +1000

    added a do-while loop for the prompts to confirm the type of char

--- assets/script.js
+++ assets/script.js
@@ -18,10 +18,27 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
+
   // Choose a length between 8 to 128 characters
   var length = window.prompt("How many characters would you like?\nEnter a number between 8 and 128");
   while ((length < 8) || (length > 128)) {
     var length = window.prompt("Please, Enter a number between 8 and 128");
   }
+
+  // Confirm to include lowercase,uppercase,numbers or special characters
+  do {
+    var isLowercase = window.confirm("Click OK to confirm for including lowercase ");
+    if (isLowercase) { passwordCreate = lowercase; }
+    var isUppercase = window.confirm("Click OK to confirm for including uppercase ");
+    if (isUppercase) { passwordCreate += uppercase; }
+    var isSet_number = window.confirm("Click OK to confirm for including numbers ");
+    if (isSet_number) { passwordCreate += set_number; }
+    var isSpcchar = window.confirm("Click OK to confirm for including special characters ");
+    if (isSpcchar) { passwordCreate += special_char; }
+  }
+
+  // Atleast one character type should be included 
+  while ((!isLowercase) && (!isUppercase) && (!isSet_number) && (!isSpcchar))
+
 
 }
